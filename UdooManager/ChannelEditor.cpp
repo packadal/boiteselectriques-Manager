43bf09c5e3269c4421ea765550acd47472c47d64
packadal <packadal@gmail.com>
2018-03-01 22:48:37 +0100

    changed track index to start at 1 in the UI

--- UdooManager/ChannelEditor.cpp
+++ UdooManager/ChannelEditor.cpp
@@ -1,27 +1,27 @@
 #include "ChannelEditor.h"
-#include "ui_ChannelEditor.h"
 #include <QFileDialog>
+#include "ui_ChannelEditor.h"
 
-ChannelEditor::ChannelEditor(QWidget *parent) :
-	QWidget(parent),
-	ui(new Ui::ChannelEditor)
-{
-	ui->setupUi(this);
+ChannelEditor::ChannelEditor(QWidget* parent)
+    : QWidget(parent), ui(new Ui::ChannelEditor) {
+  ui->setupUi(this);
 }
 
-ChannelEditor::~ChannelEditor()
-{
-	delete ui;
+ChannelEditor::~ChannelEditor() {
+  delete ui;
 }
 
-void ChannelEditor::chooseFile()
-{
-	QFile f{QFileDialog::getOpenFileName(this,
-												"Choisir un fichier audio",
-												QString(),
-												"Audio file (*.wav *.mp3 *.ogg *.flac)")};
-	if(f.exists())
-	{
-		setFilename(f.fileName()); // QFileInfo(f).fileName());
-	}
+void ChannelEditor::setBox(int arg) {
+  m_box = arg;
+  emit boxChanged(m_box);
+  ui->box_id->setNum(m_box + 1);
+}
+
+void ChannelEditor::chooseFile() {
+  QFile f{
+      QFileDialog::getOpenFileName(this, "Choisir un fichier audio", QString(),
+                                   "Audio file (*.wav *.mp3 *.ogg *.flac)")};
+  if (f.exists()) {
+    setFilename(f.fileName());  // QFileInfo(f).fileName());
+  }
 }
--- UdooManager/ChannelEditor.h
+++ UdooManager/ChannelEditor.h
@@ -4,82 +4,57 @@
 #include <QWidget>
 
 namespace Ui {
-	class ChannelEditor;
+class ChannelEditor;
 }
 
-class ChannelEditor : public QWidget
-{
-		Q_OBJECT
+class ChannelEditor : public QWidget {
+  Q_OBJECT
 
-		Q_PROPERTY(QString filename READ getFilename WRITE setFilename)
-		Q_PROPERTY(QString name READ getName WRITE setName)
-		Q_PROPERTY(int box READ getBox WRITE setBox)
-	public:
-		explicit ChannelEditor(QWidget *parent = 0);
-		~ChannelEditor();
+  Q_PROPERTY(QString filename READ getFilename WRITE setFilename)
+  Q_PROPERTY(QString name READ getName WRITE setName)
+  Q_PROPERTY(int box READ getBox WRITE setBox)
+ public:
+  explicit ChannelEditor(QWidget* parent = 0);
+  ~ChannelEditor();
 
-		QString getFilename() const
-		{
-			return m_filename;
-		}
+  QString getFilename() const { return m_filename; }
 
-		int getBox() const
-		{
-			return m_box;
-		}
+  int getBox() const { return m_box; }
 
-		QString getName() const
-		{
-			return m_name;
-		}
+  QString getName() const { return m_name; }
 
-	signals:
-		void filenameChanged(QString);
-		void boxChanged(int);
-		void nameChanged(QString);
+ signals:
+  void filenameChanged(QString);
+  void boxChanged(int);
+  void nameChanged(QString);
 
-	public slots:
-		void setFilename(QString arg)
-		{
-			m_filename = arg;
-			emit filenameChanged(m_filename);
-		}
+ public slots:
+  void setFilename(QString arg) {
+    m_filename = arg;
+    emit filenameChanged(m_filename);
+  }
 
-		void setBox(int arg)
-		{
-			m_box = arg;
-			emit boxChanged(m_box);
-		}
+  void setBox(int arg);
 
-		void setName(QString arg)
-		{
-			m_name = arg;
-			emit nameChanged(m_name);
-		}
+  void setName(QString arg) {
+    m_name = arg;
+    emit nameChanged(m_name);
+  }
 
-	private slots:
-		void priv_setName(QString arg)
-		{
-			m_name = arg;
-		}
+ private slots:
+  void priv_setName(QString arg) { m_name = arg; }
 
-		void priv_setFilename(QString arg)
-		{
-			m_filename = arg;
-		}
+  void priv_setFilename(QString arg) { m_filename = arg; }
 
-		void priv_setBox(int arg)
-		{
-			m_box = arg;
-		}
+  void priv_setBox(int arg) { m_box = arg; }
 
-		void chooseFile();
+  void chooseFile();
 
-	private:
-		Ui::ChannelEditor *ui;
-		QString m_filename{};
-		int m_box{};
-		QString m_name{};
+ private:
+  Ui::ChannelEditor* ui;
+  QString m_filename{};
+  int m_box{};
+  QString m_name{};
 };
 
-#endif // CHANNELEDITOR_H
+#endif  // CHANNELEDITOR_H
